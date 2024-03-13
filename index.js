5ccabee0b17f059d4e95cda49ab0c8ca123fbe6c
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-03-13 10:15:23 +0000

    p

--- index.js
+++ index.js
@@ -3,27 +3,86 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-
 	const server = Hapi.server({
-		port: 3000,
+		port: 9000,
 		host: 'localhost'
 	});
+	const { nanoid } = await import('nanoid');
 
-	server.route({
-		method: 'GET',
-		path: '/',
-		handler: (request, h) => {
+	server.route([
+		{
+			method: 'POST',
+			path: '/books',
+			handler: (request, h) => {
+				// Handle storing book data
+				return h.response({
+					status: 'success',
+					message: 'Buku berhasil ditambahkan',
+					data: {
+						bookId: nanoid(16)
+					}
+				}).code(201);
+			}
+		},
+		{
+			method: 'GET',
+			path: '/books',
+			handler: (request) => {
+				const { name, reading, finished } = request.query;
+				const books = [
+					{
+						id: nanoid(16),
+						name: 'Book A',
+						publisher: 'Publisher A',
+						reading: true,
+						finished: false
+					},
+					{
+						id: nanoid(16),
+						name: 'Book B',
+						publisher: 'Publisher B',
+						reading: false,
+						finished: true
+					}
+				];
 
-			return 'Hello World!';
+				let filteredBooks = books;
+
+				if (name) {
+					filteredBooks = filteredBooks.filter(book =>
+						book.name.toLowerCase().includes(name.toLowerCase())
+					);
+				}
+
+				if (reading !== undefined) {
+					const isReading = reading === '1';
+					filteredBooks = filteredBooks.filter(book =>
+						book.reading === isReading
+					);
+				}
+
+				if (finished !== undefined) {
+					const isFinished = finished === '1';
+					filteredBooks = filteredBooks.filter(book =>
+						book.finished === isFinished
+					);
+				}
+
+				return {
+					status: 'success',
+					data: {
+						books: filteredBooks
+					}
+				};
+			}
 		}
-	});
+	]);
 
 	await server.start();
 	console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
 	console.log(err);
 	process.exit(1);
 });
